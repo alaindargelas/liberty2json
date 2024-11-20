3f4758ead8312215adb0e5bc6bb7c73c83bbb896
Akash Levy <akash@silimate.com>
2024-11-20 10:55:19 -0800

    Add syntax check and exception catching

--- liberty2json.cpp
+++ liberty2json.cpp
@@ -6,12 +6,24 @@ int main(int argc, char *argv[]) {
 	LibertyParser *parser;
 	switch (argc) {
 		case 2:
-			parser = new LibertyParser(argv[1]);
-			std::cout << parser->as_json().dump(2) << std::endl;
+			try {
+				parser = new LibertyParser(argv[1]);
+				parser->check();
+				std::cout << parser->as_json().dump(2) << std::endl;
+			} catch (std::exception &e) {
+				std::cerr << "Error: " << e.what() << std::endl;
+				return 1;
+			}
 			break;
 		case 3:
+		try {
 			parser = new LibertyParser(argv[1]);
+			parser->check();
 			parser->to_json_file(argv[2]);
+		} catch (std::exception &e) {
+			std::cerr << "Error: " << e.what() << std::endl;
+			return 1;
+		}
 			break;
 		default:
 			std::cerr << "Usage: " << argv[0] << " <liberty_file> [output_file]" << std::endl;
